cc7cecadb9e15eb15b7a75709883e16917c1e2ad
itsmexta777 <kenniamexta@gmail.com>
2026-02-10 17:51:58 -0800

    editar el README.md

--- sesion_01.py
+++ sesion_01.py
@@ -1,4 +1,4 @@
-# Numeros 
+# Numeros
 print(int(7))
 print(float(7.7))
 type(7)
@@ -7,3 +7,56 @@ print(int(1+2))
 print(int(10*2))
 print(int(1+4-2))
 print(float(1+2.0))
+# operadores matematicos
+# +
+#
+# *
+#/
+#**
+# % Modulo
+
+print (int (2**3))
+print (int (4**8))
+print (float(10%3))
+print (float (25%4))
+print (float (16%2))
+print (float(10/3))
+
+# Variables
+
+print("=====variable=====")
+
+x= 100
+y = 1
+print (x + y)
+
+ventas = 1999991
+print ("Nuestras ventas fueron:",ventas)
+
+is_active = True
+print(is_active)
+game_over = False
+
+
+print(game_over)
+
+
+some_string = "hola soy string"
+print (some_string)
+
+
+print("=====condicionales=====")
+edad = 15
+if (edad >= 18):
+    print("si puedes entrar a el bar ")
+else:
+    print("no puedes entrar a el bar")
+
+
+    mi_numero =int(input("cual es el numero que deseas verificar?"))
+    print(f"el numero que deseas verificar es{mi_numero}")
+    if mi_numero % 2 ==0:
+        print(f"el numero{mi_numero} es par ")
+    else:
+        print(f"el numero {mi_numero} es in par")
+print(par_inpar(mi_numero))
