7fba37c0fd3438a755cbf2013212a6cfb7877689
itsmexta777 <kenniamexta@gmail.com>
2026-02-11 16:25:21 -0800

    archivo readme.md

---
+++ sesion_02.py
@@ -0,0 +1,24 @@
+# tuplas 
+mi_tupla =(2,4)
+print("mi tupla",mi_tupla)
+
+#lista 
+mi_lista=[1,3.1416 , "mexta", mi_tupla]
+print("el primer elemento de mi lista",mi_lista[0])
+print("el cuarto elemento de mi lista",mi_lista[3])
+print("el tercer elemento de mi lista",mi_lista[2])
+
+#diccionarios
+ 
+mi_diccionario = {"mi_lista": mi_lista ,
+                  "nombre" : "mexta"
+                  "pi" : 3.1416,
+                  "tel" : "664858662"}
+      
+
+
+print("llave para accesar a mi diccinario mi_lista",mi_diccionario["mi_lista"])
+print("llave para accesar a mi diccinario pi", mi_diccionario["pi"])
+print("llave para accesar a mi diccinario tel",mi_diccionario["tel"])
+      
+
