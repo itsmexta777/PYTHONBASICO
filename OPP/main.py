17a63f28f2f732789d3c987d7f7072b4870638de
itsmexta777 <kenniamexta@gmail.com>
2026-03-02 16:06:54 -0800

    guardar nuevamente readme.md 02032026

--- OPP/main.py
+++ OPP/main.py
@@ -1,9 +1,10 @@
 from Enemigo import *
 from zombie import *
 from ogro import *
+import random
 
 zombie = Zombie(10, 1)
-ogro = ogro(20, 3)
+ogro = Ogro(20, 3)
 
 def batalla(e1: Enemigo, e2: Enemigo):
     e1.habla()
@@ -14,7 +15,7 @@ def batalla(e1: Enemigo, e2: Enemigo):
         e1.ataque_especial()
         e2.ataque_especial()
         print(f"{e1.get_tipo_enemigo()}: queda: {e1.puntos_energia} puntos de energia!!!!")
-        print(f"{e2.get_tipo_enemigo()}: quedan: {e2.tipo_enemigo} puntos de energia!!!!!!")
+        print(f"{e2.get_tipo_enemigo()}: quedan: {e2.puntos_energia} puntos de energia!!!!!!")
         print(f"Ataque: {e1.ataque}")
         e1.puntos_energia -= e2.ataque
         print("=====================")
@@ -25,11 +26,10 @@ def batalla(e1: Enemigo, e2: Enemigo):
     if e1.puntos_energia > 0:
         print(f"{e1.get_tipo_enemigo()} ganooo!!")
     else:
-
         print(f"{e2.get_tipo_enemigo()} ganooo!!")
 
-    print("===============Batalla===============")
-    batalla(zombie, ogro)
-    print("===============Fin de la batalla")
+print("===============Batalla===============")
+batalla(zombie, ogro)
+print("===============Fin de la batalla")
 #print(f"{zombie.get_tipo_enemigo()} tiene {zombie.puntos_energia} de energia y atacra con {zombie.ataque}")
 #print(f"{ogro.get_tipo_enemigo()} tiene {ogro.puntos_energia} de energia y ataca con {ogro.ataque}")
